d56f3e583d86d3f1fee038aa650075e30a628afe
jmx <695205458@qq.com>
2019-04-02 20:39:15 +0800

    生成器，裴波拉契算法，可迭代，迭代器，iter()将可迭代类转换成迭代器，闭包，装饰器

--- 第二阶段/0402/demo15.py
+++ 第二阶段/0402/demo15.py
@@ -1,1 +1,22 @@
-import logging
+import time,datetime
+
+class Ly():
+    def __init__(self,fun):
+        self.fun=fun
+        print('这是第一步',datetime.datetime.now())
+        time.sleep(1)
+        self.fun()
+
+    def __call__(self):
+        print('这是第三步',datetime.datetime.now())
+        time.sleep(1)
+
+
+@Ly
+def show():
+    print('这是第二步',datetime.datetime.now())
+    time.sleep(1)
+
+
+show()
+
